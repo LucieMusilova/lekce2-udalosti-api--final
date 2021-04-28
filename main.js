8fca8d076f34ff425173f12ce00415351fb17333
Lucie Musilová <musil.lucie@seznam.cz>
2021-04-28 19:27:58 +0200

    predelane na EventListenery

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 // tady je místo pro náš program
 
+let veta = document.querySelector('#js-paragraph');
+let tlacitkoCervene = document.querySelector('.cerveny');
+let tlacitkoZvetsi = document.querySelector('.vetsi');
+let tlacitkoPrehraj = document.querySelector('.prehraj');
+let tlacitkoPauzni = document.querySelector('.pauzni');
+let tlacitkoZtis = document.querySelector('.ztis');
+let tlacitkoStredni = document.querySelector('.middle');
+let tlacitkoMax = document.querySelector('.max');
+let tlacitkoZacatek = document.querySelector('.start');
+let zvuk = document.querySelector('#js-audio');
+
+/*tučnější písmo při najetí myší*/
+
+veta.addEventListener('mouseover',ztucni);
+veta.addEventListener('mouseout',normalni);
+
+function ztucni() {
+  veta.style.fontWeight = 'bold';
+};
+
+function normalni() {
+  veta.style.fontWeight = 'normal';
+};
+
+
 /**
  * Funkce přidá nebo odebere na prvku třídu, která se stará o červenou barvu písma
  * 
  * @param {string} elementId 
  */
-function prepniZcervenani(elementId) {
-    document.getElementById(elementId).classList.toggle('cerveny-text');
+
+tlacitkoCervene.addEventListener('click', prepniZcervenani);
+
+function prepniZcervenani() {
+    veta.classList.toggle('cerveny-text');
 }
 
 /**
@@ -15,25 +43,33 @@ function prepniZcervenani(elementId) {
  * @param {string} elementSelector 
  * @param {int} fontSizeChange 
  */
-function zvetsitPismo(elementSelector, fontSizeChange) {
-    let element = document.querySelector(elementSelector);
-    let currentFontSize = parseInt(element.style.fontSize);
-    element.style.fontSize = (currentFontSize + fontSizeChange) + 'px';
+
+ tlacitkoZvetsi.addEventListener('click', zvetsitPismo);
+
+function zvetsitPismo() {
+    let currentFontSize = parseInt(veta.style.fontSize);
+    veta.style.fontSize = (currentFontSize + 1) + 'px';
 }
 
 /*--------------*/
 /* AUDIO FUNKCE */
 /*--------------*/
 
-function prehraj(elementSelector) {
-    document.querySelector(elementSelector).play();
-}
-function pauzni(elementSelector) {
-    document.querySelector(elementSelector).pause();
-}
-function upravHlasitost(elementSelector, volumeValue) {
-    document.querySelector(elementSelector).volume = volumeValue;
-}
-function resetuj(elementSelector) {
-    document.querySelector(elementSelector).currentTime = 0;
-}
+tlacitkoPrehraj.addEventListener('click', function() {
+  zvuk.play();
+});
+tlacitkoPauzni.addEventListener('click', function() {
+  zvuk.pause();
+});
+tlacitkoZtis.addEventListener('click', function() {
+  zvuk.volume = 0;
+});
+tlacitkoStredni.addEventListener('click', function() {
+  zvuk.volume = 0.5;
+});
+tlacitkoMax.addEventListener('click', function() {
+  zvuk.volume = 1;
+});
+tlacitkoZacatek.addEventListener('click', function() {
+  zvuk.currentTime = 0;
+});
